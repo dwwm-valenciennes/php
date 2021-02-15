5ece64376aacbe821fc6220e13a8ff411b9b974b
Matthieu Mota <matthieumota@gmail.com>
2021-02-15 09:54:04 +0100

    Continue tableau php

--- 04-tableaux/index.php
+++ 04-tableaux/index.php
@@ -13,6 +13,10 @@
         // Un tableau simple contient des chaines
         $contacts = ['Toto titi', 'Tata titi'];
 
+        // Afficher Tata titi
+        echo '<h2>Le contact sur l\'index 1</h2>';
+        echo $contacts[1];
+
         // <?= équivaut à <?php echo
         // <?php $contact;
 
@@ -21,10 +25,29 @@
         // Un tableau peut contenir N dimensions.
         // Ici, on en a 3: Les contacts, Le contact, Les adresses.
         $contactMultis = [
-            ['nom' => 'Titi', 'prenom' => 'Toto', 'adresses' => ['A', 'B']],
+            [
+                'nom' => 'Titi',
+                'prenom' => 'Toto',
+                'adresses' => [
+                    0 => '18, rue de la Paix, Paris',
+                    1 => '17, avenue du Palais, Cambrai'
+                ]
+            ],
+            [
+                'nom' => 'Titi',
+                'prenom' => 'Tata',
+                'adresses' => ['18, rue de la Paix, Paris']
+            ]
         ];
     ?>
 
+    <h2>Afficher le prénom et le nom du second contact</h2>
+    <?php echo $contactMultis[1]['prenom'].' '.$contactMultis[1]['nom']; ?>
+
+    <h2>Afficher la première adresse de Toto Titi</h2>
+    <?php echo $contactMultis[0]['adresses'][0]; ?>
+
+    <h2>La liste des contacts dans le tableau multidimensionnel</h2>
     <?php foreach ($contactMultis as $contact) { ?>
 
         <div>
@@ -37,21 +60,6 @@
         </div>
 
     <?php } ?>
-    
-    <div>
-        <h1>Toto Titi</h1>
-        <ul>
-            <li>18, rue de la Paix, Paris</li>
-            <li>17, avenue du Palais, Cambrai</li>
-        </ul>
-    </div>
-
-    <div>
-        <h1>Tata Titi</h1>
-        <ul>
-            <li>18, rue de la Paix, Paris</li>
-        </ul>
-    </div>
 
 </body>
 </html>
