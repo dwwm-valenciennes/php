e563364fca6025e34b10001abdb016aca3fe997c
Matthieu Mota <matthieumota@gmail.com>
2021-03-11 17:08:17 +0100

    Form post

--- 12-ajax/02-form/post.php
+++ 12-ajax/02-form/post.php
@@ -1,3 +1,22 @@
 <?php
 
-var_dump($_POST);
+// On ralentit le script volontairement pour
+// voir le loader
+sleep(1);
+
+$message = $_POST['message'] ?? null;
+$errors = [];
+
+if (strlen($message) < 5) {
+    $errors['message'] = 'Le message est trop court';
+}
+
+if (empty($errors)) {
+    // Si tout va bien, on renvoie simplement le message
+    echo json_encode(['message' => $message]);
+} else {
+    // S'il y a des erreurs, on les renvoie pour les récupérer
+    // côté JS
+    http_response_code(422);
+    echo json_encode(['errors' => $errors]);
+}
