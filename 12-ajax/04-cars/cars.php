649377e5ad65e3a395529624890a9ef1000cd9d3
Matthieu Mota <matthieumota@gmail.com>
2021-03-15 12:14:59 +0100

    wip ajax

--- 12-ajax/04-cars/cars.php
+++ 12-ajax/04-cars/cars.php
@@ -1,6 +1,7 @@
 <?php
 
-$db = new PDO('mysql:host=localhost;dbname=cars', 'root', '', [
+// charset=UTF8 permet de corriger le soucis des accents
+$db = new PDO('mysql:host=localhost;dbname=cars;charset=UTF8', 'root', '', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 ]);
@@ -10,7 +11,7 @@ $id = $_GET['id'] ?? null;
 if ($id) {
     $query = $db->prepare('SELECT * FROM cars WHERE id = :id');
     $query->execute(['id' => $id]);
-    $cars = $query->fetch();
+    $cars = $query->fetch(); // On récupère 1 SEULE voiture
 } else {
     $cars = $db->query('SELECT * FROM cars')->fetchAll();
 }
