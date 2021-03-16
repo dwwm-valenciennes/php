598d610cdae0b4e4f37e5ad5c07c727adef46a35
Matthieu Mota <matthieumota@gmail.com>
2021-03-16 16:17:39 +0100

    Comment

--- 09-mysql/database.sql
+++ 09-mysql/database.sql
@@ -78,6 +78,80 @@ CREATE TABLE IF NOT EXISTS `webflix`.`movie_has_actor` (
 ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `webflix`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `webflix`.`user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `role` VARCHAR(255) NULL DEFAULT 'user',
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `webflix`.`order`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `webflix`.`order` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `created_at` DATETIME NOT NULL,
+  `paid` TINYINT NOT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_order_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_order_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `webflix`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `webflix`.`order_movies`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `webflix`.`order_movies` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `format` VARCHAR(255) NOT NULL,
+  `order_id` INT NOT NULL,
+  `movie_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_order_movies_order1_idx` (`order_id` ASC),
+  INDEX `fk_order_movies_movie1_idx` (`movie_id` ASC),
+  CONSTRAINT `fk_order_movies_order1`
+    FOREIGN KEY (`order_id`)
+    REFERENCES `webflix`.`order` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_order_movies_movie1`
+    FOREIGN KEY (`movie_id`)
+    REFERENCES `webflix`.`movie` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `webflix`.`comment`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `webflix`.`comment` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `pseudo` VARCHAR(255) NOT NULL,
+  `message` LONGTEXT NOT NULL,
+  `note` INT NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `movie_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_comment_movie1_idx` (`movie_id` ASC),
+  CONSTRAINT `fk_comment_movie1`
+    FOREIGN KEY (`movie_id`)
+    REFERENCES `webflix`.`movie` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
